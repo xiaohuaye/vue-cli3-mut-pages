26c593445c8e8422cc02d9cccb686a987aa6d0e9
xiaohuaye <908347816@qq.com>
2019-02-20 04:15:52 +0800

    发现build后cart没有使用模板，这与dev的时候并不相同

--- vue.config.js
+++ vue.config.js
@@ -3,27 +3,27 @@ module.exports = {
   pages: {
     index: {
       // 页面的入口文件
-      entry: 'src/index/index.js',
+      entry: 'src/pages/index/index.js',
       // 页面的模板文件
-      template: 'public/index.html',
+      template: 'src/public/index.html',
       // build 生成的文件名称  例： dist/index.html
       filename: 'index.html'
     },
     // template 默认会去找 public/subpage.html 页面，如果找不到会使用 public/index.html 文件
     // 输出文件会默认的推断为 subpage.html
     subpage: {
-      entry: 'src/subpage/subpage.js',
-      template: 'public/subpage.html',
+      entry: 'src/pages/subpage/subpage.js',
+      template: 'src/public/subpage.html',
     },
     cart: {
-      entry: 'src/cart/cart.js',
+      entry: 'src/pages/cart/cart.js',
     },
   },
   devServer: {
     historyApiFallback: {
       rewrites: [
         { from: /\/index/, to: '/index.html' },
-        { from: /\/cart/, to: '/subpage.html' },
+        { from: /\/cart/, to: '/cart.html' },
         { from: /\/subpage/, to: '/subpage.html' }
       ]
     }
